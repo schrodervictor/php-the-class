e2b919380b1f2bda870a32d56e15129d64258e7e
Victor Schröder <schrodervictor@gmail.com>
2017-06-04 03:14:47 +0200

    Adds working method for preg_match (argument passed by reference)

--- lib/PHP.php
+++ lib/PHP.php
@@ -2,6 +2,13 @@
 
 class PHP
 {
+    public static function preg_match(
+        $pattern, $subject, &$matches = null, $flags = 0, $offset = 0
+    ) {
+        return preg_match($pattern, $subject, $matches, $flags, $offset);
+    }
+
+
     public static function __callStatic($method, $arguments)
     {
         return call_user_func_array($method, $arguments);
--- tests/unit/PHPTest.php
+++ tests/unit/PHPTest.php
@@ -11,4 +11,29 @@ class PHPTest extends TestCase
 
         $this->assertSame($expected, $result);
     }
+
+
+    public function test_preg_match_withTwoArgs()
+    {
+        $pattern = '/(mate)/';
+        $subject = 'club-mate at 2:36';
+
+        $expected = preg_match($pattern, $subject);
+        $result = PHP::preg_match($pattern, $subject);
+
+        $this->assertSame($expected, $result);
+    }
+
+
+    public function test_preg_match_withThreeArgs()
+    {
+        $pattern = '/(mate)/';
+        $subject = 'club-mate at 2:36';
+
+        $expected = preg_match($pattern, $subject, $matchesExpected);
+        $result = PHP::preg_match($pattern, $subject, $matchesResult);
+
+        $this->assertSame($expected, $result);
+        $this->assertEquals($matchesExpected, $matchesResult);
+    }
 }
